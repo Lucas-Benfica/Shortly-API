83d11996836f7f865b86173743dcfd66a4a82ad8
Lucas Soares Benfica <128869796+Lucas-Benfica@users.noreply.github.com>
2023-08-03 15:29:01 -0300

    SignIn & SingUp functions

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,18 +1,34 @@
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
+import { db } from "../database/databaseConnection.js";
 
+export async function signUp(req, res) {
+    const { name, email, password, confirmPassword } = req.body;
 
-export async function cadastro(req, res) {
-    const {name, email, password, confirmPassword} = req.body;
+    if (password !== confirmPassword) {
+        return res.status(409).send({ message: "Incompatible passwords" });
+    }
+
     try {
-
-        //const userOk = await db.collection('user').findOne({email});
-        //if (userOk) return res.sendStatus(409);
         const hash = bcrypt.hashSync(password, 10);
-        
-        //await db.collection('user').insertOne({name, email, password: hash, adress});
-        res.sendStatus(201);
+
+        await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`, [name, email, hash]);
+
+        res.status(201).json({ message: "User registered successfully" });
     } catch (error) {
-        res.status(500).send(error.message);
+        res.status(500).send("Error while signing up: " + error.message);
+    }
+}
+
+export async function signIn(req, res){
+    const {email, password} = req.body;
+    const {userData} = res.locals;
+    const token = uuid();
+    try{
+        await db.query(`INSERT INTO session ("userId", token) VALUES ($1, $2);`, [userData.id, token]);
+
+        res.status(200).send({token: token, userName: userData.name});
+    }catch (err) {
+        res.status(500).send("Error while signing in: " + error.message);  
     }
 }
