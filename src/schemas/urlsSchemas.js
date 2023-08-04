6b534424c0ce2af6144f2efeda8969af66df862f
Lucas-Benfica <lucassoaresbenfica@gmail.com>
2023-08-04 14:24:23 -0300

    updates

--- src/schemas/urlsSchemas.js
+++ src/schemas/urlsSchemas.js
@@ -1,7 +1,6 @@
 import Joi from "joi";
 
 export const schemaUrl = Joi.object({
-    url: Joi.string().required()
+    url: Joi.string().uri({ scheme: ['http', 'https'] }).required()
 });
 
-// url: Joi.string().uri({ scheme: ['https'] }).required()
